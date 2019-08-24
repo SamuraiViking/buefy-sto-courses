53eede940aeb7000154ef85a55fcde0ac84cea70
Kevin <nelson67@stolaf.edu>
2019-08-24 16:52:23 -0500

    add pagination

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
+import axios from 'axios'
 import 'buefy/dist/buefy.css'
+import 'vue-material-design-icons/styles.css';
 
 Vue.use(Buefy)
+
 Vue.config.productionTip = false
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/"
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,61 +1,148 @@
 <template>
   <div class="home">
-    <b-table 
-      :data="data" 
-      :columns="columns"
-      :mobile-cards="true"
+    {{ someModel }}
+    <b-table class="my-table"
+      :data="data"
+      bordered
+      mobile-cards
+      paginated
+      per-page="10"
+      narrowed
+      stripped
     >
+    <template slot-scope="props">
+      <b-table-column label="Status" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.status }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Name" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.name }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Dept" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.dept_num_sec }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Gereqs" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.gereqs }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Days" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.days }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Times" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.times }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Prof" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.prof }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Rating" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.rating }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Difficulty" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.difficulty }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Reviews" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.reviews }}</span>
+         </template>
+      </b-table-column>
+      <b-table-column label="Prereqs" centered>
+         <template v-if="props.index === 0">
+           <b-input v-model.trim="someModel" size="is-small"></b-input>
+         </template>
+         <template v-else>
+           <span>{{ props.row.has_prereqs }}</span>
+         </template>
+      </b-table-column>      
+      <b-table-column label="Actions" centered>
+         <template v-if="props.index !== 0">
+            <button>Add</button>
+            <button>View</button>
+         </template>
+      </b-table-column>
+    </template>
     </b-table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'home',
   components: {
-
+  },
+  created() {
+    axios.get('api/courses?term=20191&type=class').then(response => {
+      this.data = response.data.courses
+    })
   },
   data() {
     return {
-      data: [
-        { 'id': 1, 'first_name': 'Jesse', 'last_name': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
-        { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
-        { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
-        { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male' },
-        { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female' }
-      ],
-      columns: [
-        {
-            field: 'id',
-            label: 'ID',
-            width: '40',
-            numeric: true
-        },
-        {
-            field: 'first_name',
-            label: 'First Name',
-        },
-        {
-            field: 'last_name',
-            label: 'Last Name',
-        },
-        {
-            field: 'date',
-            label: 'Date',
-            centered: true
-        },
-        {
-            field: 'gender',
-            label: 'Gender',
-        }
-      ]
+      data: [],
+      someModel: '',
+      top: 'top'
     }
+  },
+  methods: {
   }
 }
 </script>
 
-<style>
+<style link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
 .home {
-  margin: 30px;
+  padding: 30px;
+  /*max-width: 1000px;*/
+  max-height: 500px;
+  margin: 0px auto;
+}
+
+.my-table {
+  max-height: 50%;
+  overflow-y: auto;
 }
 </style>
